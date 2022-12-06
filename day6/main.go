3b1c9685adccea877ff49d295c666b5847f2a080
Kevin Glowacz <kjg@github.com>
2022-12-06 10:28:19 -0600

    Day6 Part2

--- day6/main.go
+++ day6/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	Part1()
+	input, _ := os.ReadFile("input.txt")
+	stream := string(input)
+
+	fmt.Println("Part 1: ", markerAfter(stream, 4))
+	fmt.Println("Part 1: ", markerAfter(stream, 14))
+}
+
+func markerAfter(stream string, uniqueNeeded int) int {
+	markerAt := 0
+	for i := 0; i < len(stream)-uniqueNeeded; i++ {
+		marker := stream[i : i+uniqueNeeded]
+
+		if IsUnique(marker) {
+			markerAt = i + uniqueNeeded
+			break
+		}
+	}
+	return markerAt
+}
+
+func IsUnique(s string) bool {
+	uniqMap := make(map[rune]bool)
+	for _, c := range s {
+		if uniqMap[c] {
+			return false
+		}
+		uniqMap[c] = true
+	}
+	return true
 }
