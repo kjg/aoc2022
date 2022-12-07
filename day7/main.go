b1ee68b421406d971b597c3d895eaa5a1127b12f
Kevin Glowacz <kjg@github.com>
2022-12-07 15:26:50 -0600

    Day7 Part1

---
+++ day7/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	Part1()
+}
+
+func Part1() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	dirSizes := make(map[string]int)
+	currentDir := ""
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		cmdRegex := regexp.MustCompile(`^\$ (\w+)\s?(.*)$`)
+		cmdMatch := cmdRegex.FindStringSubmatch(line)
+
+		sizeRegex := regexp.MustCompile(`^(\d+)`)
+		sizeMatch := sizeRegex.FindStringSubmatch(line)
+
+		if len(cmdMatch) > 0 && cmdMatch[1] == "cd" {
+			currentDir = changeDir(cmdMatch[2], currentDir)
+		} else if len(sizeMatch) > 0 {
+			size, _ := strconv.Atoi(sizeMatch[1])
+			addSizetoDir(currentDir, dirSizes, size)
+		}
+	}
+
+	fmt.Println("Part1: Total of dirs", totalOfDirsUnder(100001, dirSizes))
+}
+
+func changeDir(newDir string, currentDir string) string {
+	switch newDir {
+	case "/":
+		return ""
+	case "..":
+		dirArr := strings.Split(currentDir, "/")
+		return strings.Join(dirArr[:len(dirArr)-1], "/")
+	}
+	return currentDir + "/" + newDir
+}
+
+func addSizetoDir(dir string, dirSizes map[string]int, size int) {
+	dirArr := strings.Split(dir, "/")
+	for i := 0; i < len(dirArr); i++ {
+		dir := strings.Join(dirArr[:i+1], "/")
+		currentSize := dirSizes[dir]
+		dirSizes[dir] = currentSize + size
+	}
+}
+
+func totalOfDirsUnder(size int, dirSizes map[string]int) int {
+	total := 0
+	for _, dirSize := range dirSizes {
+		if dirSize < size {
+			total += dirSize
+		}
+	}
+	return total
+}
