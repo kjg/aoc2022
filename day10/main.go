8f8f7bf10e3dee5e56d6a18b8578574cfb2a932a
Kevin Glowacz <kjg@github.com>
2022-12-11 11:38:50 -0600

    Day10 Part2

--- day10/main.go
+++ day10/main.go
@@ -16,6 +16,7 @@ func main() {
 
 	instructions := strings.Split(stream, "\n")
 	instructions = instructions[0 : len(instructions)-1]
+	screen := make([]string, 6)
 
 	commandRegex := regexp.MustCompile(`^(\w+)\s?(.*)$`)
 
@@ -23,23 +24,26 @@ func main() {
 	xRegister := 1
 
 	for _, instruction := range instructions {
-		cycle = tick(cycle, xRegister)
+		cycle = tick(cycle, xRegister, screen)
 		parsedInstruction := commandRegex.FindStringSubmatch(instruction)
 		cmd := parsedInstruction[1]
 
 		switch cmd {
 		case "noop":
 		case "addx":
-			cycle = tick(cycle, xRegister)
+			cycle = tick(cycle, xRegister, screen)
 			value, _ := strconv.Atoi(parsedInstruction[2])
 
 			xRegister = xRegister + value
 		}
 	}
 	fmt.Println("strengthSum", strengthSum)
+	for _, line := range screen {
+		fmt.Println(line)
+	}
 }
 
-func tick(cycle int, xRegister int) int {
+func tick(cycle int, xRegister int, screen []string) int {
 	cycle = cycle + 1
 	fmt.Println("starting cycle", cycle, "with register value", xRegister)
 	if shouldDoInspection(cycle) {
@@ -47,9 +51,25 @@ func tick(cycle int, xRegister int) int {
 		strengthSum += strength
 		fmt.Println("strenth", strength)
 	}
+
+	drawScreen(cycle, xRegister, screen)
+
 	return cycle
 }
 
 func shouldDoInspection(cycle int) bool {
 	return (cycle-20)%40 == 0
+}
+
+func drawScreen(cycle int, spritePosition int, screen []string) {
+	colNumber := (cycle - 1) % 40
+	rowNumber := (cycle - 1) / 40
+
+	pixel := "."
+	fmt.Println("cycle", cycle, "colNum", colNumber, "sprite", spritePosition)
+
+	if colNumber >= spritePosition-1 && colNumber <= spritePosition+1 {
+		pixel = "#"
+	}
+	screen[rowNumber] = screen[rowNumber] + pixel
 }
